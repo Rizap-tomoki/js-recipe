c8919f0411c4012e654a431b16cfeac4a407823e
tomoki.sato <tomoki.sato@rizapgroup.com>
2023-04-20 19:25:57 +0900

    [Trello (保存機能なし)]を学習しました。

---
+++ 02_レシピ/02_クイズ（複数）/main.js
@@ -0,0 +1,143 @@
+const quizText = document.getElementById("quiz-text")
+const quizImage = document.getElementById("quiz-image")
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const feedback = document.getElementById("feedback")
+const nextQuizButton = document.getElementById("next-quiz")
+
+let quizNumber = 0
+const quizzes = [
+  {
+    text: "この星の名前は何でしょう？",
+    image: "Ganymede.jpg",
+    choices: [
+      {
+        text: "ゴリアテ",
+        isCorrect: false,
+        feedback:
+          "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+      },
+      {
+        text: "ゼニガメ",
+        isCorrect: false,
+        feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+      },
+      {
+        text: "ガニメデ",
+        isCorrect: true,
+        feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+      },
+    ],
+  },
+  {
+    text: "いま、何問目？",
+    image: "Two.jpeg",
+    choices: [
+      {
+        text: "１",
+        isCorrect: false,
+        feedback: "残念！死刑",
+      },
+      {
+        text: "２",
+        isCorrect: true,
+        feedback: "正解！てか画像でわかるやん",
+      },
+      {
+        text: "３",
+        isCorrect: false,
+        feedback: "残念！死刑",
+      },
+    ],
+  },
+  {
+    text: "この城の名前は？",
+    image: "Maruoka.png",
+    choices: [
+      {
+        text: "ただ城",
+        isCorrect: true,
+        feedback: "残念死刑",
+      },
+      {
+        text: "普通城",
+        isCorrect: true,
+        feedback: "残念死刑。",
+      },
+      {
+        text: "サナ城",
+        isCorrect: false,
+        feedback: "正解！どこからどうみても丸岡城だね。",
+      },
+    ],
+  },
+]
+// 共通の処理①
+// quizNumber番目の問題を画面に表示する
+const reloadQuiz = function (quizNumber) {
+    // quizzes[quizNumber] は使いまわすので、定数に入れておく
+    const quiz = quizzes[quizNumber]
+  
+    // 問題文を表示
+    quizText.textContent = "Q. " + quiz.text
+  
+    // 画像を表示
+    quizImage.src = "./images/" + quiz.image
+  
+    // 選択肢（ボタン）の中身を表示
+    choice1.textContent = quiz.choices[0].text
+    choice2.textContent = quiz.choices[1].text
+    choice3.textContent = quiz.choices[2].text
+  
+    // フィードバックを削除
+    feedback.textContent = ""
+  
+    // 次の問題ボタンを隠す
+    nextQuizButton.classList.add("hidden")
+  }
+  
+  // 共通の処理②
+  // choiceNumber番目の選択肢を選択
+  const choose = function (choiceNumber) {
+    // quizzes[quizNumber].choices[choiceNumber] は使いまわすので、定数に入れておく
+    const choice = quizzes[quizNumber].choices[choiceNumber]
+  
+    // フィードバックを表示
+    feedback.textContent = choice.feedback
+  
+    // 正解ならば……
+    if (choice.isCorrect) {
+      if (quizNumber < quizzes.length - 1) {
+        // 次の問題があれば、次の問題ボタンを表示
+        nextQuizButton.classList.remove("hidden")
+      } else {
+        // 次の問題がなければ、結果を表示
+      }
+    }
+  }
+  
+  choice1.onclick = function () {
+    // 0番目の選択肢を選択
+    choose(0)
+  }
+  choice2.onclick = function () {
+    // 1番目の選択肢を選択
+    choose(1)
+  }
+  choice3.onclick = function () {
+    // 2番目の選択肢を選択
+    choose(2)
+  }
+  
+  // 次の問題ボタンを押したら
+  nextQuizButton.onclick = function () {
+    // 問題番号を１増やす
+    quizNumber += 1
+    // quizNumber番目の問題を読み込む
+    reloadQuiz(quizNumber)
+  }
+  
+  // 0番目のクイズを表示
+  reloadQuiz(0)
+  
--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,38 +1,43 @@
 const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
-const choice1 = document.getElementById("choice-1")
-const choice2 = document.getElementById("choice-2")
-const choice3 = document.getElementById("choice-3")
+const element = document.getElementById("element")
+const choice1 = document.createElement("button")
+const choice2 = document.createElement("button")
+const choice3 = document.createElement("button")
 const feedback = document.getElementById("feedback")
 
 // クイズの内容
 const quiz = {
-  text: "この星の名前は何でしょう？",
-  image: "Ganymede.jpg",
+  text: "これは誰でしょう",
+  image: "Two.jpeg",
   choices: [
     {
-      text: "ゴリアテ",
-      feedback:
-        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+      text: "サナりゅ",
+      feedback: "残念！サナリュはもっと",
     },
     {
-      text: "ゼニガメ",
-      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+      text: "ともキング",
+      feedback: "正解！ともキングです",
     },
     {
-      text: "ガニメデ",
-      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+      text: "さ〜〜や",
+      feedback: "惜しい！でもさやはもっと怖い",
     },
   ],
 }
 
 // quiz を画面に表示する関数
-const reloadQuiz = function() {
+const reloadQuiz = function () {
   // 問題文を表示
   quizText.textContent = "Q. " + quiz.text
 
   // 画像を表示
   quizImage.src = "./images/" + quiz.image
+
+  // 追加
+  element.append(choice1)
+  element.append(choice2)
+  element.appendChild(choice3)
 
   // 選択肢（ボタン）の中身を表示
   choice1.textContent = quiz.choices[0].text
@@ -41,20 +46,20 @@ const reloadQuiz = function() {
 }
 
 // choiceNumber番目の選択肢を選択
-const choose = function(choiceNumber) {
+const choose = function (choiceNumber) {
   // フィードバックを表示
   feedback.textContent = quiz.choices[choiceNumber].feedback
 }
 
-choice1.onclick = function() {
+choice1.onclick = function () {
   // 0 番目の選択肢を選択
   choose(0)
 }
-choice2.onclick = function() {
+choice2.onclick = function () {
   // 1 番目の選択肢を選択
   choose(1)
 }
-choice3.onclick = function() {
+choice3.onclick = function () {
   // 2 番目の選択肢を選択
   choose(2)
 }
